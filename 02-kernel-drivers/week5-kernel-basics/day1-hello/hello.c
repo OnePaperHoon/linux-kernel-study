81b22f0228579cdfeb53c00d1c9865535d904bfe
OnePaperHoon <wkdgns720@naver.com>
2026-03-29 21:55:08 +0900

    add hello module

--- 02-kernel-drivers/week5-kernel-basics/day1-hello/hello.c
+++ 02-kernel-drivers/week5-kernel-basics/day1-hello/hello.c
@@ -1,35 +1,33 @@
-#include <linux/init.h>			// __init, __exit
-#include <linux/module.h>		// module_init, module_exit
-#include <linux/kernel.h>		// printk
-
+#include "linux/printk.h"
+#include <linux/init.h>   // __init, __exit
+#include <linux/kernel.h> // printk
+#include <linux/module.h> // module_init, module_exit
 
 /*
-	모듈 초기화 함수
-	- insmod 실행 시 자동 호출
-	- 성공 0반환
-	- 실패 음수 반환
+        모듈 초기화 함수
+        - insmod 실행 시 자동 호출
+        - 성공 0반환
+        - 실패 음수 반환
 */
-static int __init hello_init(void)
-{
-	printfk(KERN_INFO "===================================\n");
-	printfk(KERN_INFO "Hello, Kernel Module!\n");
-	printfk(KERN_INFO "Week 5 Day 1: Module Basics\n");
-	printfk(KERN_INFO "Author: OnePaperHoon\n");
-	printfk(KERN_INFO "===================================\n");
-	return 0;
+static int __init hello_init(void) {
+  printk(KERN_INFO "===================================\n");
+  printk(KERN_INFO "Hello, Kernel Module!\n");
+  printk(KERN_INFO "Week 5 Day 1: Module Basics\n");
+  printk(KERN_INFO "Author: OnePaperHoon\n");
+  printk(KERN_INFO "===================================\n");
+  return 0;
 }
 
 /*
-	모듈 종료 함수
-	-rmmod 실행 시 자동 호출
-	-리소스 정리
+        모듈 종료 함수
+        -rmmod 실행 시 자동 호출
+        -리소스 정리
 */
-static __exit hello_exit(void)
-{
-	printk(KERN_INFO "====================================\n");
-    printk(KERN_INFO "Goodbye, Kernel Module!\n");
-    printk(KERN_INFO "Module unloaded successfully.\n");
-    printk(KERN_INFO "====================================\n");
+static void __exit hello_exit(void) {
+  printk(KERN_INFO "====================================\n");
+  printk(KERN_INFO "Goodbye, Kernel Module!\n");
+  printk(KERN_INFO "Module unloaded successfully.\n");
+  printk(KERN_INFO "====================================\n");
 }
 
 module_init(hello_init);
@@ -38,4 +36,4 @@ module_exit(hello_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("OnePaperHoon");
 MODULE_DESCRIPTION("Simple Hello World Kernel Module - Week 5 Day 1");
-MOUDLE_VERSION("1.0");
+MODULE_VERSION("1.0");
